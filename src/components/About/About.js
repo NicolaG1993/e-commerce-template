b21a4ba007797f5966e6210938c447b4548fc978
Nicola Gaioni <nicog.designer@gmail.com>
2021-07-01 15:15:41 +0200

    About page good setup

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,7 +1,35 @@
+import { Link } from "react-router-dom";
+import text from "./data/text";
+
 export default function About() {
     return (
-        <section>
-            <h2>ABOUT</h2>
+        <section id="aboutus">
+            <div className="aboutus-comp-div aboutus-box1">
+                <div className="aboutus-box1-text">
+                    <h1>{text.block1.heading}</h1>
+                    <p>{text.block1.p_1}</p>
+                    <p>{text.block1.p_2}</p>
+                </div>
+            </div>
+            <div className="aboutus-comp-div aboutus-box2">
+                <div className="aboutus-box2-text">
+                    <h2>{text.block2.heading}</h2>
+                    <p>{text.block2.p_1}</p>
+                    <p>{text.block2.p_2}</p>
+                    <p>{text.block2.p_3}</p>
+                    <Link to={"/contact"}>Contattaci!</Link>
+                </div>
+            </div>
+            <div className="aboutus-comp-div aboutus-box3">
+                <div className="aboutus-box3-text">
+                    <h2>{text.block3.heading}</h2>
+                    <p>{text.block3.p_1}</p>
+                    <p>{text.block3.p_2}</p>
+                </div>
+            </div>
+            <div className="aboutus-comp-div aboutus-box4">
+                <h2>{text.block4.heading}</h2>
+            </div>
         </section>
     );
 }
---
+++ src/components/About/data/text.js
@@ -0,0 +1,23 @@
+export default {
+    block1: {
+        heading: "Welcome to the future",
+        p_1: "Volevi vendere dei giocattoli mai usati di tuo figlio, o quel vestitino ancora nuovo che ormai non gli va piú bene?",
+        p_2: "Oppure stai facendo acquisti per il tuo bambino ma vorresti anche risparmiare in sicurezza? Niente di piú facile!",
+    },
+    block2: {
+        heading: "Who we are",
+        p_1: "Nasce Da Mamy a Mamy, il mercatino dell'usato dei genitori, per gli acquisti per i vostri bambini.",
+        p_2: "Il nostro negozio ha aperto nel 2019 a Cavaion Veronese, ed ha subito attirato l'attenzione di genitori e famiglie, rivelandosi un'interessante novitá nella provincia di Verona; Non solo perché si possono trovare prodotti di qualitá in ottime condizioni, ma anche perché i nostri clienti possono rivendere i loro articoli trammite di noi, lasciandoci prendere cura delle trattative e dei loro prodotti.",
+        p_3: "Da Mamy a Mamy infatti nasce dall'idea di Sara Gagliardi, due volte mamma, che sa quindi riconoscere al meglio le esigenze delle proprie clienti, sempre disponibile a dare consiglio sul prossimo acquisto per i piú piccoli",
+    },
+    block3: {
+        heading: "Staff",
+        p_1: "Lorem Ipsum",
+        p_2: "Lorem Ipsum",
+    },
+    block4: {
+        heading: "Welcome to the future",
+        p_1: "Lorem Ipsum",
+        p_2: "Lorem Ipsum",
+    },
+};
